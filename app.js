aac30591b1cabcf4f76cd9859bcfde4bcb126a30
Calvin Wong <cwong@gmail.com>
2016-12-21 23:51:16 -0800

    Attempting user table creation

--- app.js
+++ app.js
@@ -32,11 +32,15 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
 
+  // client
+  //   .query('SELECT table_schema,table_name FROM information_schema.tables;')
+  //   .on('row', function(row) {
+  //     console.log(JSON.stringify(row));
+  //   });
   client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
+    .query('CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(40) not null)')
+    .on('end', () => { client.end(); });
+
 });
 
 /*
